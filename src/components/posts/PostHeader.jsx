326f202298eeb676e18a2169a3cca8af4ce64120
sidneisimeao <sidneisimmon@gmail.com>
2018-09-16 13:04:38 -0300

    Ajustes

--- src/components/posts/PostHeader.jsx
+++ src/components/posts/PostHeader.jsx
@@ -4,7 +4,7 @@ const PostHeader = () => (
     <div className="header-content">
         <img src="http://placehold.it/900x200" />
         <h1 className="title is-4">
-            Sidnei Simmon to Make Python Slower
+            Sidnei Simmon to Make React+Bulma
 		</h1>
         <small><i>Posted in <a href="category.html">Web Design</a> on <a href="post.html">January 20, 2015</a> by <a href="category.html">Author</a></i></small>
     </div>
