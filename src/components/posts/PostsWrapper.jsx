8dff6f54de12771a5a259737ec21c955ce93a6aa
sidneisimeao <sidneisimmon@gmail.com>
2018-09-16 10:26:21 -0300

    teste

--- src/components/posts/PostsWrapper.jsx
+++ src/components/posts/PostsWrapper.jsx
@@ -4,10 +4,7 @@ import Post from 'components/posts/Post';
 
 const PostsWrapper = () => (
     <div className="column is-8 is-offset-1 is-centered">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post />        
     </div>
 );
 
